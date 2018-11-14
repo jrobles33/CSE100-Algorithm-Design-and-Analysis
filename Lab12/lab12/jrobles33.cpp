cfd979ba8b6ec4d5a3fc1f3e6a33bb8727253c6a
root <root@localhost.localdomain>
2018-11-14 12:20:12 -0800

    works for all test cases. Use an online compiler because the grader potentially does not work

--- Lab12/lab12/jrobles33.cpp
+++ Lab12/lab12/jrobles33.cpp
@@ -1,62 +1,33 @@
 #include <iostream>
-
-
 using namespace std;
-
-/*
-int Bottom_Up_Cut_Rod(int p[], int n){
-int r[n];
-r[0]=0;
-for (int j = 0; j < n; j++){
-	int q = min();
-	for (int i = 0; i < j; i++){
-		q = max(q, p[i] + r[j-i]);
-		}
-	r[j] = q;
-	}
-return r[n];
-} */
 
 int Extended_Bottom_Up_Cut_Rod(int p[], int n){
 
-int r[n];
-int s[n];
-r[0] = 0;
-
+int rev[n];
+int slice[n];
+rev[0] = 0;
 for (int j = 1; j <= n; j++){
-	int q = -9999;
+	int max = -9999;
 	for (int i = 1; i <= j; i++){
-		if (q < (p[i] + r[j-i])){
-		q =(p[i] + r[j-i]);
-		s[j] = i;
+		int comparison = (p[i] + rev[j-i]);
+		if (max < comparison){
+		max = comparison;
+		slice[j] = i;
 		}
+		
 		}
-	r[j] = q;
+	rev[j] = max;
 	}
-cout<<r[n]<<endl;
+cout<<rev[n]<<endl;
 while( n > 0){
-	cout<<s[n]<<" ";
-	n = n-s[n];
+	cout<<slice[n]<<" ";
+	n = n-slice[n];
 	}
 cout<<"-1"<<endl;
-return r[n];
-return s[n];
+return rev[n];
+return slice[n];
 
 }
-/*
-void Print_Cut_Rod_Solution(int p[], int n){
-int r[n];
-int s[n];
-(r,s) = Extended_Bottom_Up_Cut_Rod(p, n);
-while (n > 0){
-	cout <<s[n]<< " ";
-	n = n-s[n];
-	}
-}
-*/
-
-
-
 
 
 int main(){
@@ -69,6 +40,14 @@ int prices[rod_length];
 for(int i = 1; i <= rod_length; i ++){
 cin>>prices[i];
 }
+
+//testing for any changes
+/*cout<<endl;
+cout<<"Prices check"<<endl;
+for(int i = 1; i <= rod_length; i ++){
+cout<<prices[i];
+}*/
+//end test
 
 Extended_Bottom_Up_Cut_Rod(prices, rod_length);
 //Print_Cut_Rod_Solution(prices, rod_length);
